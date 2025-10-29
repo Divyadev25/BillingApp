6d2f303d51d17df7c68d1f5a95012ce0ccdb7974
Divya_dev <divyaannamalai27@gmail.com>
2025-10-29 03:20:22 -0700

    Add files via upload

---
+++ Models/MenuItem.cs
@@ -0,0 +1,13 @@
+﻿namespace RestaurantBillingApp.Models
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalAmount { get; set; }
+        public System.DateTime Date { get; set; }
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RestaurantBillingApp.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
